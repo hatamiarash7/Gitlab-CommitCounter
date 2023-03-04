a8881a588b4ad30d13fef75a00da8dc64389e66c
Arash Hatami <hatamiarash7@gmail.com>
2023-03-04 12:27:51 +0330

    Add comments

--- counter.py
+++ counter.py
@@ -2,27 +2,35 @@ from os import environ
 import sys
 import requests
 
+# Get the required environment variables
 PROJECT_ID = environ.get('PROJECT_ID')
 ACCESS_TOKEN = environ.get('ACCESS_TOKEN')
 BRANCH = environ.get('BRANCH')
 
+# Check if any of the environment variables are missing and exit if so
 if (PROJECT_ID is None) or (ACCESS_TOKEN is None) or (BRANCH is None):
     print("Missing environment variables. See https://github.com/hatamiarash7/Gitlab-CommitCounter for help.")
     sys.exit(0)
 
+# Construct the API URL using the environment variables
 url = f"https://gitlab.com/api/v4/projects/{PROJECT_ID}/repository/commits?ref_name={BRANCH}"
 
+# Send a HEAD request to the API with the authentication token
 response = requests.head(url, headers={
     "PRIVATE-TOKEN": ACCESS_TOKEN})
 
 try:
+    # Try to get the value of the x-total header from the response
     x_total = response.headers.get("x-total")
     if x_total is not None:
+        # If the value is not None, print it
         print(x_total)
     else:
+        # If the value is None, something went wrong
         print("Something is wrong !!")
         print("Check environment variables. See https://github.com/hatamiarash7/Gitlab-CommitCounter for help.")
 except Exception as e:
+    # Catch any exceptions and print an error message with the exception details
     print("Something is wrong !!")
     print("Missing environment variables. See https://github.com/hatamiarash7/Gitlab-CommitCounter for help.")
     print(f"Exception: {str(e)}")
